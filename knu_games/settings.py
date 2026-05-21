c05c7696d1428ed8d6bd285f5d0b3f855c6e33fe
Ivan Koshelia <koshelia13112006@gmail.com>
2026-05-21 15:53:03 +0300

    add main page

--- knu_games/settings.py
+++ knu_games/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'games',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'knu_games.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
