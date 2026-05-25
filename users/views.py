6afc9bc7df24e35c21a4028ab10138736f01f7c0
Artem Velychko <artemvel.ua7@gmail.com>
2026-05-25 17:39:09 +0300

    Злиття front

--- users/views.py
+++ users/views.py
@@ -1,54 +1,33 @@
+from decimal import Decimal
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import Profile
 from orders.models import OrderItem
 
-
 def register_view(request):
-
     if request.method == 'POST':
-
         username = request.POST['username']
         password = request.POST['password']
-
-        user = User.objects.create_user(
-            username=username,
-            password=password
-        )
-
+        user = User.objects.create_user(username=username, password=password)
         login(request, user)
-
         return redirect('home')
-
     return render(request, 'register.html')
 
-
 def login_view(request):
-
     if request.method == 'POST':
-
         username = request.POST['username']
         password = request.POST['password']
-
-        user = authenticate(
-            request,
-            username=username,
-            password=password
-        )
-
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-
             return redirect('home')
-
     return render(request, 'login.html')
 
-
+@login_required
 def profile_view(request):
     profile, created = Profile.objects.get_or_create(user=request.user)
-
-    # Шукаємо всі ігри з оплачених замовлень цього користувача
     purchased_items = OrderItem.objects.filter(order__user=request.user, order__is_paid=True)
     purchased_games = [item.game for item in purchased_items]
 
@@ -56,3 +35,24 @@ def profile_view(request):
         'profile': profile,
         'purchased_games': purchased_games
     })
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+@login_required
+def deposit_funds(request):
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        if amount:
+            try:
+                deposit_amount = Decimal(amount)
+                if deposit_amount > 0:
+                    # Використовуємо get_or_create для уникнення помилки DoesNotExist
+                    profile, created = Profile.objects.get_or_create(user=request.user)
+                    profile.balance += deposit_amount
+                    profile.save()
+            except ValueError:
+                pass
+
+    return redirect('profile')
