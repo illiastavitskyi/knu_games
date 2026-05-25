b6c4d86139fb8bb748eedefb86b30ee50b88c1f4
Artem Velychko <artemvel.ua7@gmail.com>
2026-05-25 17:19:16 +0300

    Merge: resolved conflicts, kept front branch features

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('profile/', views.profile_view, name='profile'),
-
     path('logout/', views.logout_view, name='logout'),
     path('deposit/', views.deposit_funds, name='deposit'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,55 +2,32 @@ from decimal import Decimal
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import Profile
 from orders.models import OrderItem
-from django.contrib.auth import logout
 
 def register_view(request):
-
     if request.method == 'POST':
-
         username = request.POST['username']
         password = request.POST['password']
-
-        user = User.objects.create_user(
-            username=username,
-            password=password
-        )
-
+        user = User.objects.create_user(username=username, password=password)
         login(request, user)
-
         return redirect('home')
-
     return render(request, 'register.html')
 
-
 def login_view(request):
-
     if request.method == 'POST':
-
         username = request.POST['username']
         password = request.POST['password']
-
-        user = authenticate(
-            request,
-            username=username,
-            password=password
-        )
-
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-
             return redirect('home')
-
     return render(request, 'login.html')
 
-
+@login_required
 def profile_view(request):
     profile, created = Profile.objects.get_or_create(user=request.user)
-
-    # Шукаємо всі ігри з оплачених замовлень цього користувача
     purchased_items = OrderItem.objects.filter(order__user=request.user, order__is_paid=True)
     purchased_games = [item.game for item in purchased_items]
 
