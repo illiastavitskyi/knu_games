eb1d65beb3b1f9cc86ef03b12a56d5015b141a95
Artem Velychko <artemvel.ua7@gmail.com>
2026-05-25 16:55:47 +0300

    Налаштування вигляду сторінок

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import register_view, login_view
 from . import views
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-path('profile/', views.profile_view, name='profile')
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile_view, name='profile'),
+
+    path('logout/', views.logout_view, name='logout'),
+    path('deposit/', views.deposit_funds, name='deposit'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
+from decimal import Decimal
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from .models import Profile
 from orders.models import OrderItem
-
+from django.contrib.auth import logout
 
 def register_view(request):
 
@@ -56,3 +58,24 @@ def profile_view(request):
         'profile': profile,
         'purchased_games': purchased_games
     })
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+@login_required
+def deposit_funds(request):
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        if amount:
+            try:
+                deposit_amount = Decimal(amount)
+                if deposit_amount > 0:
+                    # Використовуємо get_or_create для уникнення помилки DoesNotExist
+                    profile, created = Profile.objects.get_or_create(user=request.user)
+                    profile.balance += deposit_amount
+                    profile.save()
+            except ValueError:
+                pass
+
+    return redirect('profile')
