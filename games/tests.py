dfb242fafa8c6cfb6615f605c350d8f3a2aaa4fe
Ivan Koshelia <koshelia13112006@gmail.com>
2026-05-27 18:37:49 +0300

    add tests

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Game
 
-# Create your tests here.
+
+class GameTests(TestCase):
+
+    def test_game_creation(self):
+
+        game = Game.objects.create(
+            title='CS2',
+            description='Shooter game',
+            price=500,
+            release_date='2024-01-01',
+            developer='Valve'
+        )
+
+        self.assertEqual(game.title, 'CS2')
+
+
+    def test_home_page_opens(self):
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserTests(TestCase):
+
+    def test_register_page_opens(self):
+
+        response = self.client.get('/users/register/')
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_user_registration(self):
+
+        response = self.client.post('/users/register/', {
+            'username': 'testuser',
+            'password': '12345'
+        })
+
+        self.assertEqual(response.status_code, 302)
+
+        self.assertTrue(
+            User.objects.filter(username='testuser').exists()
+        )
+
+
+    def test_login_works(self):
+
+        User.objects.create_user(
+            username='testuser',
+            password='12345'
+        )
+
+        response = self.client.post('/users/login/', {
+            'username': 'testuser',
+            'password': '12345'
+        })
+
+        self.assertEqual(response.status_code, 302)
